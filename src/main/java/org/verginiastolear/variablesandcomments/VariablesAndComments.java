49133f764364068a61eb69258481fcd53e8687db
Verginia Stolear <Verginia.Stolear@gmail.com>
2023-05-09 17:13:07 +0300

    Added the Variables and Comments Task.

---
+++ src/main/java/org/verginiastolear/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,66 @@
+package org.verginiastolear.variablesandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+        // Declare the primitive data type variables
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDatatype;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        // Initialization of the above variables
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 98;
+        lastCharFromAscii = 127;
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDatatype = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+308d;
+
+        //display the primitive data type variables
+
+        System.out.println(falseBooleanVariable);
+        System.out.println(trueBooleanVariable);
+        System.out.println(firstCharFromAscii);
+        System.out.println(lastCharFromAscii);
+        System.out.println(lowerLimitOfByteDataType);
+        System.out.println(upperLimitOfByteDataType);
+        System.out.println(lowerLimitOfShortDataType);
+        System.out.println(upperLimitOfShortDataType);
+        System.out.println(lowerLimitOfIntDataType);
+        System.out.println(upperLimitOfIntDataType);
+        System.out.println(lowerLimitOfLongDataType);
+        System.out.println(upperLimitOfLongDataType);
+        System.out.println(lowerLimitOfFloatDatatype);
+        System.out.println(upperLimitOfFloatDataType);
+        System.out.println(lowerLimitOfDoubleDataType);
+        System.out.println(upperLimitOfDoubleDataType);
+
+
+    }
+}
